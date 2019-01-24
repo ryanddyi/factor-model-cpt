706e864dfaec6075b644abdedc68b4473876ce3d
Ryan Yi <ryan.ddyi@gmail.com>
2019-01-24 09:00:36 -0800

    .

--- cpt_functions.py
+++ cpt_functions.py
@@ -13,28 +13,28 @@ import pandas as pd
 
 def cost_function(eta, s2, f2_mat, k_plus):
     n = f2_mat.shape[0]
-    
+
     cost=0
     for k in range(k_plus):
         a = eta*s2 + f2_mat[:,k].sum()
         b = eta+2+n
         lambda2_hat = a/b
         cost = cost + 0.5*(a/lambda2_hat + b*math.log(lambda2_hat))
-    
+
     return cost
 
 # max log-likelihood cost function with flat prior
 
 def cost_function_no_prior(f2_mat, k_plus):
     n = f2_mat.shape[0]
-    
+
     cost=0
     for k in range(k_plus):
         a = f2_mat[:,k].sum()
         b = n
         lambda2_hat = a/b
         cost = cost + 0.5*(a/lambda2_hat + b*math.log(lambda2_hat))
-    
+
     return cost
 
 # max log-likelihood cost function with scaled inverse chi-square prior
@@ -43,7 +43,7 @@ def cost_function_no_prior(f2_mat, k_plus):
 def cost_function_inactive_factors(eta, s2, f2_mat):
     n = f2_mat.shape[0]
     k_plus = f2_mat.shape[1]
-    
+
     a = eta*s2
     b = eta+2
     for k in range(k_plus):
@@ -51,57 +51,57 @@ def cost_function_inactive_factors(eta, s2, f2_mat):
         b = b + n
     lambda2_hat = a/b
     cost = 0.5*(a/lambda2_hat + b*math.log(lambda2_hat))
-    
+
     return cost
 
 # change-point detection functions
 # PELT algorithm is applied with a pre-determined candidate cpt_set
 
 def cpt_detect_PELT(eta, s2, f2_mat, k_plus, cpt_set):
-    
+
     # change-point candidate sets: tau_sets
     n_all = len(cpt_set) - 1
-    
+
     pen = 0.5*k_plus*math.log(n_all)
     lastchangecpts = np.ndarray(n_all+1, dtype='int');
     lastchangelike = np.ndarray(n_all+1);
     numchangecpts = np.ndarray(n_all+1, dtype='int');
     checklist = np.ndarray(n_all+1, dtype='int');
     tmplike = np.ndarray(n_all+1);
-    
+
     # initialize F(0) = -penalty, cp(0) = NULL
-    
+
     lastchangelike[0] = -pen;
     lastchangecpts[0] = 0;
     numchangecpts[0] = 0;
-    
+
     # F(1) = C(y_0), cp(1) = 0,
     lastchangelike[1] = cost_function(eta, s2, f2_mat[cpt_set[0]:cpt_set[1],:], k_plus);
     lastchangecpts[1] = 0;
     numchangecpts[1] = 1;
-    
+
     nchecklist=2;
     checklist[0] = 0;
     checklist[1] = 1;
-    
+
     for tstar in range(2, n_all+1):
         for i in range(nchecklist):
             tmplike[i] = (lastchangelike[checklist[i]]
                           + cost_function(eta, s2, f2_mat[cpt_set[checklist[i]]:cpt_set[tstar],:], k_plus)
                           + pen);
-        
+
         whichout = tmplike[range(nchecklist)].argmin()
         minout = tmplike[whichout]
         lastchangelike[tstar] = minout
         lastchangecpts[tstar] = checklist[whichout]
         numchangecpts[tstar] = numchangecpts[lastchangecpts[tstar]]+1;
-        
+
         nchecktmp=0;
         for i in range(nchecklist):
             if tmplike[i]<=(lastchangelike[tstar]+pen):
                 checklist[nchecktmp] = checklist[i]
                 nchecktmp = nchecktmp+1
-    
+
         nchecklist = nchecktmp
         checklist[nchecklist] = tstar
         nchecklist = nchecklist+1
